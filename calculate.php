7171346b9f376fd0ac05c09c547335ebba3d0aac
ephillips-tx <ephillips.tx@gmail.com>
2014-07-06 16:29:23 -0500

    Update calculate.php

--- calculate.php
+++ calculate.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+  1. generate forms for entry by user
+     based on number of incomes provided
+  2. receive user input for pay frequency 
+     and income amount
+  3. send information to results page for 
+     processing
+*/
+
 $num_incomes = $_GET["num"];
 
 include("header.php");
@@ -9,18 +18,18 @@ include("header.php");
 <form action="results.php" method="get">
 
 <?php 
-  for( $inc in $num_incomes  ){ 
+$count = 1;
+for( $inc in $num_incomes  ){ 
 ?>
-    <select class="frequencies">
-      <option value="Weekly" selected></option>
-      <option value="Biweekly"></option>
-      <option value="Semimonthly"></option>
-      <option value="Monthly"></option>
-    </select>
-    <input class="income" type="number" name="income" size="5" min="0">
+  <select class="frequencies" name="frequency<?php print $count; ?>">
+    <option value="Weekly" selected></option>
+    <option value="Biweekly"></option>
+    <option value="Semimonthly"></option>
+    <option value="Monthly"></option>
+  </select>
+  <input class="income" type="number" name="income<?php print $count; ?>" size="5" min="0">
 <?php } ?>
-
-<input type="submit" value="Calculate">
+  <input type="submit" value="Calculate">
 </form>
 
 <?php include("footer.php"); ?>
