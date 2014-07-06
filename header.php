dfda03d732ff8cb9d8ff6544dbe198586b719acf
ephillips-tx <ephillips.tx@gmail.com>
2014-07-06 14:25:25 -0500

    header.php
    This is the header file

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+  HEADER.PHP FILE
+*/
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="script.js"></script>
+    <meta name="viewport" content="width=device-width">
+    <title>Pay Frequency Calculator By Eric Phillips</title>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <h1 id="logo">E.P.</h1>
+      </header>
+      
