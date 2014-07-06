ef9195b585c32f9510989c2769d8293191f2fc14
ephillips-tx <ephillips.tx@gmail.com>
2014-07-06 16:06:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,14 @@ include("functions.php");
 ?>
 
 <div class="square">
-  
+  <content>
+    How many incomes do you wish to calculate?
+    <br>
+    <form action="calculate.php" method="get">
+      <input type="number" name="num" required>
+      <input type="submit" name="submit" value="Next">
+    </form>
+  <content>
 </div>
 <div class="square">
 
