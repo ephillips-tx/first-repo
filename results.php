f0b70131f7056f91a4db9e01335d3bd7e31c5a27
ephillips-tx <ephillips.tx@gmail.com>
2014-07-06 16:39:12 -0500

    Update results.php

--- results.php
+++ results.php
@@ -1,7 +1,15 @@
 <?php
 
-include("header.php");
+/*
+  1. store frequencies and incomes in array
+  2. calculate largest income and store that and frequency
+  3. convert all other incomes to correct frequency
+  4. add all incomes
+  5. print result
+*/
 
+
+include("header.php");
 
 print "<p>The customer receives the " . $frequency . " amount of " . $income . "</p>";
 include("footer.php");
