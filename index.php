cdcaee5e1f5cb9b52bbd5939e9c6d93976a0c5a0
ephillips-tx <ephillips.tx@gmail.com>
2014-07-06 16:21:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,17 @@
 
 /*
   This is the main content file
+  1. prompt user for number of incomes
+  2. send number of incomes and produce 
+     form on entry page
 */
+
 include("functions.php");
 include("header.php");
 
 ?>
 
-<div class="square">
+<div class="container">
   <content>
     How many incomes do you wish to calculate?
     <br>
@@ -17,9 +21,6 @@ include("header.php");
       <input type="submit" name="submit" value="Next">
     </form>
   <content>
-</div>
-<div class="square">
-
 </div>
 
 <?php include("footer.php"); ?>
