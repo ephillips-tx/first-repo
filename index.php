c9ed5c4795f71349352645f62bba4a64e01fcf4e
ephillips-tx <ephillips.tx@gmail.com>
2014-07-06 16:19:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 /*
   This is the main content file
 */
-
-include("header.php");
-include("footer.php");
 include("functions.php");
+include("header.php");
 
 ?>
 
@@ -23,3 +21,5 @@ include("functions.php");
 <div class="square">
 
 </div>
+
+<?php include("footer.php"); ?>
